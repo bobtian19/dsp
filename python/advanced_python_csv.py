9702da7e0256706ea4cf7fb6d3458a7a54baebf6
bobtian19 <bobtian19@gmail.com>
2016-09-02 15:38:41 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+import csv
+f = open('faculty.csv') 
+next(f) #skip first line
+with open('output.csv', "wb") as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')    
+    for line in f:
+        email = line.split(',')[-1].rstrip()
+        writer.writerow([email])
