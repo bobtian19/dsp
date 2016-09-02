c7a6ecf6b44d46c73cb3ffacaabf54afce6553ae
bobtian19 <bobtian19@gmail.com>
2016-09-02 12:07:00 -0700

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,15 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+f = open('football.csv') 
+next(f) #skip first line
+l_team = []
+l_absgd = []
+#populate list of team names and list of corresponding absolute goal difference
+for line in f:
+    temp = line.split(',')
+    l_team.append(temp[0])
+    l_absgd.append(abs(int(temp[5]) - int(temp[6])))
+
+i = l_absgd.index(min(l_absgd))
+print(l_team[i] + ' has the smallest absolute goal difference')
